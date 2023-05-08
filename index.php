6d5a2e321be0dd60fac4b47247166b8d866ca0e2
Jabed Hasan <60073739+JabedHossainJabuu@users.noreply.github.com>
2023-05-09 02:14:35 +0600

    9th May 2023

--- index.php
+++ index.php
@@ -18,36 +18,67 @@
 
         <div class="container">
                 <!-- modal -->
-                <div class="modal fade" id="usermodal" tabindex="-1" aria-labelledby="exampleModalLabel"
-                        aria-hidden="true">
+                <div class="modal fade" id="usermodal" aria-labelledby="exampleModalLabel" aria-hidden="true">
                         <div class="modal-dialog">
                                 <div class="modal-content">
                                         <div class="modal-header">
-                                                <h5 class="modal-title" id="exampleModalLabel">Add new user</h5>
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                        aria-label="Close"></button>
+                                                <h5 class="modal-title" id="exampleModalLabel">Update users</h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                         </div>
-                                        <div class="modal-body">
-                                                <form>
-                                                        <div class="mb-3">
-                                                                <label for="name" class="col-form-label">Name:</label>
-                                                                <input type="text" class="form-control" id="name">
+                                        <form id="addform" method="POST" enctype="multipart/form-data">
+                                                <div class="modal-body">
+
+                                                        <div class="form-group">
+                                                                <label for="username">Name: </label>
+                                                                <div class="input-group">
+                                                                        <div class="input-group-prepend">
+                                                                                <span class="input-group-text bg-dark">
+                                                                                        <i class="fas fa-user-alt text-light"></i>
+                                                                                </span>
+                                                                        </div>
+                                                                        <input type="text" class="form-control" placeholder="Enter your username" required="required" id="username" name="username">
+                                                                </div>
+                                                        </div>     
+                                                        
+                                                        <div class="form-group">
+                                                                <label for="name">Name: </label>
+                                                                <div class="input-group">
+                                                                        <div class="input-group-prepend">
+                                                                                <span class="input-group-text bg-dark">
+                                                                                        <i class="fas fa-envelope-open text-light"></i>
+                                                                                </span>
+                                                                        </div>
+                                                                        <input type="email" class="form-control" placeholder="Enter your email" required="required" id="email" name="email">
+                                                                </div>
+                                                        </div> 
+
+                                                        <div class="form-group">
+                                                                <label for="mobile">Mobile: </label>
+                                                                <div class="input-group">
+                                                                        <div class="input-group-prepend">
+                                                                                <span class="input-group-text bg-dark">
+                                                                                        <i class="fas fa-phone text-light"></i>
+                                                                                </span>
+                                                                        </div>
+                                                                        <input type="text" class="form-control" placeholder="Enter your mobile" required="required" id="mobile" name="mobile" maxlength="12" minlength="10">
+                                                                </div>
+                                                        </div> 
+
+                                                        <div class="form-group">
+                                                                <label for="photo" class="custom-file-label">Choose File: </label>
+                                                                <div class="input-group">
+                                                                        <input type="file" class="custom-file-input" name="photo" required="required" id="photo">
+                                                                </div>
                                                         </div>
-                                                        <div class="mb-3">
-                                                                <label for="email" class="col-form-label">Email:</label>
-                                                                <input type="email" class="form-control" id="email">
-                                                        </div>
-                                                        <div class="mb-3">
-                                                                <label for="phone" class="col-form-label">Phone:</label>
-                                                                <input type="tel" class="form-control" id="phone">
-                                                        </div>
-                                                </form>
-                                        </div>
-                                        <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary"
-                                                        data-bs-dismiss="modal">Close</button>
-                                                <button type="button" class="btn btn-primary">Save changes</button>
-                                        </div>
+
+                                                </div>
+
+                                                <div class="modal-footer">
+                                                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                                                        <button type="button" class="btn btn-dark">Submit</button>
+                                                </div>
+                                        </form>
+
                                 </div>
                         </div>
                 </div>
@@ -63,8 +94,7 @@
                                 </div>
                         </div>
                         <div class="col-2">
-                                <button class="btn btn-dark" type="button" data-bs-toggle="modal"
-                                        data-bs-target="#usermodal">Add new user</button>
+                                <button class="btn btn-dark" type="button" data-bs-toggle="modal" data-bs-target="#usermodal">Add new user</button>
                         </div>
                 </div>
         </div>
