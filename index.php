1ecd533f81c64ffdf70f299bf3a7e44edc153d74
Jabed Hasan <60073739+JabedHossainJabuu@users.noreply.github.com>
2023-05-10 21:55:28 +0600

    10 th may 2023

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php 
+
+require_once 'partials/connect.php';
+$dbobj=new Database();
+var_dump($dbobj);
+
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -23,7 +34,8 @@
                                 <div class="modal-content">
                                         <div class="modal-header">
                                                 <h5 class="modal-title" id="exampleModalLabel">Update users</h5>
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                        aria-label="Close"></button>
                                         </div>
                                         <form id="addform" method="POST" enctype="multipart/form-data">
                                                 <div class="modal-body">
@@ -33,49 +45,66 @@
                                                                 <div class="input-group">
                                                                         <div class="input-group-prepend">
                                                                                 <span class="input-group-text bg-dark">
-                                                                                        <i class="fas fa-user-alt text-light"></i>
+                                                                                        <i
+                                                                                                class="fas fa-user-alt text-light"></i>
                                                                                 </span>
                                                                         </div>
-                                                                        <input type="text" class="form-control" placeholder="Enter your username" required="required" id="username" name="username">
+                                                                        <input type="text" class="form-control"
+                                                                                placeholder="Enter your username"
+                                                                                required="required" id="username"
+                                                                                name="username">
                                                                 </div>
-                                                        </div>     
-                                                        
+                                                        </div>
+
                                                         <div class="form-group">
                                                                 <label for="name">Name: </label>
                                                                 <div class="input-group">
                                                                         <div class="input-group-prepend">
                                                                                 <span class="input-group-text bg-dark">
-                                                                                        <i class="fas fa-envelope-open text-light"></i>
+                                                                                        <i
+                                                                                                class="fas fa-envelope-open text-light"></i>
                                                                                 </span>
                                                                         </div>
-                                                                        <input type="email" class="form-control" placeholder="Enter your email" required="required" id="email" name="email">
+                                                                        <input type="email" class="form-control"
+                                                                                placeholder="Enter your email"
+                                                                                required="required" id="email"
+                                                                                name="email">
                                                                 </div>
-                                                        </div> 
+                                                        </div>
 
                                                         <div class="form-group">
                                                                 <label for="mobile">Mobile: </label>
                                                                 <div class="input-group">
                                                                         <div class="input-group-prepend">
                                                                                 <span class="input-group-text bg-dark">
-                                                                                        <i class="fas fa-phone text-light"></i>
+                                                                                        <i
+                                                                                                class="fas fa-phone text-light"></i>
                                                                                 </span>
                                                                         </div>
-                                                                        <input type="text" class="form-control" placeholder="Enter your mobile" required="required" id="mobile" name="mobile" maxlength="12" minlength="10">
+                                                                        <input type="text" class="form-control"
+                                                                                placeholder="Enter your mobile"
+                                                                                required="required" id="mobile"
+                                                                                name="mobile" maxlength="12"
+                                                                                minlength="10">
                                                                 </div>
-                                                        </div> 
+                                                        </div>
 
                                                         <div class="form-group">
-                                                                <label for="photo" class="custom-file-label">Choose File: </label>
+                                                                <label for="photo" class="custom-file-label">Choose
+                                                                        File: </label>
                                                                 <div class="input-group">
-                                                                        <input type="file" class="custom-file-input" name="photo" required="required" id="photo">
+                                                                        <input type="file" class="custom-file-input"
+                                                                                name="photo" required="required"
+                                                                                id="photo">
                                                                 </div>
                                                         </div>
 
                                                 </div>
 
                                                 <div class="modal-footer">
-                                                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
                                                         <button type="button" class="btn btn-dark">Submit</button>
+                                                        <button type="button" class="btn btn-danger"
+                                                                data-bs-dismiss="modal">Close</button>
                                                 </div>
                                         </form>
 
@@ -94,8 +123,44 @@
                                 </div>
                         </div>
                         <div class="col-2">
-                                <button class="btn btn-dark" type="button" data-bs-toggle="modal" data-bs-target="#usermodal">Add new user</button>
+                                <button class="btn btn-dark" type="button" data-bs-toggle="modal"
+                                        data-bs-target="#usermodal">Add new user</button>
                         </div>
+
+                        <table class="table mt-5" id="usertable">
+                                <thead class="table-dark">
+                                        <tr>
+                                                <th scope="col">Image</th>
+                                                <th scope="col">Name</th>
+                                                <th scope="col">Email</th>
+                                                <th scope="col">Phone</th>
+                                                <th scope="col">Operations</th>
+                                        </tr>
+                                </thead>
+                                <tbody>
+                                        <tr>
+                                                <th scope="row">Picture1</th>
+                                                <td>Demo</td>
+                                                <td>Demo@gmail.com</td>
+                                                <td>0123</td>
+                                                <td>
+                                                        <span>Edit</span>
+                                                        <span>Profile</span>
+                                                        <span>Delete</span>
+                                                </td>
+                                        </tr>
+                                </tbody>
+                        </table>
+                        <!-- pagination -->
+                        <nav aria-label="Page navigation example" id="pagination">
+                                <ul class="pagination justify-content-center">
+                                        <li class="page-item disabled"><a class="page-link" href="#">Previous</a></li>
+                                        <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                                </ul>
+                        </nav>
                 </div>
         </div>
 
